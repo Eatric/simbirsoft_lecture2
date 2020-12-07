1fa02b9fdb07b35414948b74f0ae11fd24b360be
Eatric <iksanovk@gmail.com>
2020-12-07 23:53:47 +0300

    Modify domain models and add migrations

---
+++ SimbirSoft.API.Database/Domain/BaseEntityWithLinks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimbirSoft.API.Database.Domain
+{
+	/// <summary>
+	/// Базовый класс для слабых сущностей
+	/// </summary>
+	/// <typeparam name="TEntity1">Сущность 1</typeparam>
+	/// <typeparam name="TEntity2">Сущность 2</typeparam>
+	public abstract class BaseEntityWithLinks <TEntity1, TEntity2>
+		where TEntity1 : BaseEntity
+		where TEntity2 : BaseEntity
+	{
+		/// <summary>
+		/// Идентификатор первой связанной сущности
+		/// </summary>
+		public long Entity1Id { get; set; }
+
+		/// <summary>
+		/// Первая связанная сущность
+		/// </summary>
+		public TEntity1 Entity1 { get; set; }
+
+		/// <summary>
+		/// Идентификатор второй связанной сущности
+		/// </summary>
+		public long Entity2Id { get; set; }
+
+		/// <summary>
+		/// Вторая связанная сущность
+		/// </summary>
+		public TEntity2 Entity2 { get; set; }
+	}
+}
---
+++ SimbirSoft.API.Database/Migrations/20201207204551_AddProducerToMovie.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimbirSoft.API.Database.Migrations
+{
+    public partial class AddProducerToMovie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Producer",
+                table: "Movies",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Producer",
+                table: "Movies");
+        }
+    }
+}
